c16859cff6ac6b238b5e258543d46fcf748cb3fd
jiangmingyang <1562923681@qq.com>
2021-07-21 20:32:12 +0800

    JsonArray转list

---
+++ ibaits-swaager/src/main/java/com/jmy/mapper/PresPhotos.java
@@ -0,0 +1,15 @@
+package com.jmy.mapper;
+
+public class PresPhotos {
+
+    private String pres_photos;
+
+    public String getPres_photos() {
+        return pres_photos;
+    }
+
+    public void setPres_photos(String pres_photos) {
+        this.pres_photos = pres_photos;
+    }
+
+}
--- ibaits-swaager/src/test/java/com/jmy/ibaits/IbaitsSwaagerApplicationTests.java
+++ ibaits-swaager/src/test/java/com/jmy/ibaits/IbaitsSwaagerApplicationTests.java
@@ -1,13 +1,36 @@
 package com.jmy.ibaits;
 
+import com.alibaba.fastjson.JSONObject;
+import com.jmy.mapper.PresPhotos;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest(classes = IbaitsSwaagerApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
 class IbaitsSwaagerApplicationTests {
 
     @Test
     void contextLoads() {
+        ArrayList<PresPhotos> presPhotos = new ArrayList<>();
+        PresPhotos photo = new PresPhotos();
+        photo.setPres_photos("s4d56sa4d6sa4d6a5");
+        presPhotos.add(photo);
+
+        System.out.println(extractBase64FromJson(JSONObject.toJSONString(presPhotos)));
+    }
+
+    private String extractBase64FromJson(String jsonArray){
+        if (StringUtils.isBlank(jsonArray)) {
+            return null;
+        }
+
+        List<PresPhotos> list = JSONObject.parseArray(jsonArray, PresPhotos.class);
+        return list.isEmpty() ? null : list.get(0).getPres_photos();
     }
 
 }
